d48e8f788f34feb49c578306ddc1a8901d03a6c3
J3ST3L2 <tyler.berno89@gmail.com>
2026-05-08 08:30:28 -0400

    Make ARR 1Password render script noninteractive

--- arr-homelab/scripts/render-arr-env.sh
+++ arr-homelab/scripts/render-arr-env.sh
@@ -6,7 +6,7 @@ TOKEN_FILE="${OP_TOKEN_FILE:-/etc/1password/arr-stack.token}"
 OUT_FILE="${OUT_FILE:-/run/arr-stack.env}"
 
 export OP_SERVICE_ACCOUNT_TOKEN="$(cat "$TOKEN_FILE")"
-op inject -i "$STACK_DIR/.env.tpl" -o "$OUT_FILE"
+op inject -f -i "$STACK_DIR/.env.tpl" -o "$OUT_FILE"
 chmod 600 "$OUT_FILE"
 
 echo "Rendered $OUT_FILE from 1Password"
